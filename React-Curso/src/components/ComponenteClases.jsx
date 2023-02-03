e8eb79c229e0d113bc2d9e0b7ee71861dc36aeba
RodriiAndino <andinorodrigo03@gmail.com>
2023-02-03 10:26:11 -0300

    first commit

---
+++ React-Curso/src/components/ComponenteClases.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ComponenteClase extends Component {
+    render() {
+      return <p><strong>Hola mundo Soy un componente de Clase</strong> </p>;
+    }
+}
+
+export default ComponenteClase;
