bbb935b37e8b0e7c0b4c78b128fee9a66b80bcb0
RodriiAndino <andinorodrigo03@gmail.com>
2023-02-06 11:07:31 -0300

    2-Props 'Creado'

---
+++ 2-Props/src/components/ComponenteFuncion.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ComponeteFuncionDeclarada() {
+  return (
+    <p>
+      <strong>Hola mundo Soy un componente de Funcion Declarada</strong>{' '}
+    </p>
+  );
+}
