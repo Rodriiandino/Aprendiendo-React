75b82853b9e3bfbe666177120786872afb982ba8
RodriiAndino <andinorodrigo03@gmail.com>
2023-02-06 11:15:46 -0300

    Eliminacion de Proyecto 'Curso'

--- React-Curso/src/components/ComponenteClases.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class ComponenteClase extends Component {
-    render() {
-      return <p><strong>Hola mundo Soy un componente de Clase</strong> </p>;
-    }
-}
-
-export default ComponenteClase;
