569560fc4e8a17854b952ca1cc55842dfd656f6f
RodriiAndino <andinorodrigo03@gmail.com>
2023-02-14 22:56:36 -0300

    add: X is now added when clicked

--- 04_TicTacToe/src/App.jsx
+++ 04_TicTacToe/src/App.jsx
@@ -1,43 +1,70 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({ value, handleClick }) {
+  //Creo una funcion "square" donde recibira 2 parametros
+  //"value" Sera el atributo que imprimira el boton
+  //"handleClick" esa una funcial el cual cambiara el valor del atrubito "value" cuando el boton se aprete
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //En una variable creamos un useState en el cual se creara un Array de 9 posiciones "matrix".
+  //Ademas llenamos todo el array con "null"
+  //squares almacenara el valor y el setSquares cambiara su valor
+
+  function handleClick(i) {
+    //En la funcion handleClick() recibira un parametro el cual se usara para navegar por el array
+    const listSquares = squares.slice();
+    //El método splice() permite cambiar el contenido del arreglo eliminando o sustituyendo los elementos existentes por otros nuevos.
+    listSquares[i] = 'x';
+    setSquares(listSquares);
+  }
+
   return (
     <>
       <h1>TIC-TAC-TOE</h1>
       <section className="tictactoe">
         <table>
           <tbody>
-            <tr>
+            <tr className="board-row">
               <td className="td-bottom td-right">
-                <button className="square">1</button>
+                <Square value={squares[0]} handleClick={() => handleClick(0)} />
               </td>
               <td className="td-bottom td-right td-left">
-                <button className="square">2</button>
+                <Square value={squares[1]} handleClick={() => handleClick(1)} />
               </td>
               <td className="td-bottom td-left">
-                <button className="square">3</button>
+                <Square value={squares[2]} handleClick={() => handleClick(2)} />
               </td>
             </tr>
-            <tr>
+
+            <tr className="board-row">
               <td className="td-bottom td-right td-top">
-                <button className="square">4</button>
+                <Square value={squares[3]} handleClick={() => handleClick(3)} />
               </td>
               <td className="td-bottom td-right td-left td-top">
-                <button className="square">5</button>
+                <Square value={squares[4]} handleClick={() => handleClick(4)} />
               </td>
               <td className="td-bottom td-left td-top">
-                <button className="square">6</button>
+                <Square value={squares[5]} handleClick={() => handleClick(5)} />
               </td>
             </tr>
-            <tr>
+
+            <tr className="board-row">
               <td className="td-right td-top">
-                <button className="square">7</button>
+                <Square value={squares[6]} handleClick={() => handleClick(6)} />
               </td>
               <td className="td-right td-left td-top">
-                <button className="square">8</button>
+                <Square value={squares[7]} handleClick={() => handleClick(7)} />
               </td>
               <td className="td-left td-top">
-                <button className="square">9</button>
+                <Square value={squares[8]} handleClick={() => handleClick(8)} />
               </td>
             </tr>
           </tbody>
@@ -46,5 +73,3 @@ function App() {
     </>
   );
 }
-
-export default App;
