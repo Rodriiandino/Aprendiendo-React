34b72049ca2655d3779e7e4ad0269ceeca5ccdf7
RodriiAndino <andinorodrigo03@gmail.com>
2023-10-02 15:35:02 -0300

    add: api

---
+++ 04_SearchPlant/api/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const KEY_PLANT = process.env.KEY_PLANT
+
+app.use(cors())
+
+app.get('/plants', async (req, res) => {
+  try {
+    const page = req.query.page || 1
+    const search = req.query.search || ''
+    const url = `https://trefle.io/api/v1/plants/search?token=${KEY_PLANT}&page=${page}&q=${search}`
+    const response = await fetch(url)
+
+    const contentType = response.headers.get('content-type')
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('No se recibió una respuesta JSON válida')
+    }
+
+    const data = await response.json()
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at ${port}`)
+})
